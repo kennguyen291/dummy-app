da1124114132eca519a792218ea71e6af92c6256
Ken Nguyen <khangnguyen291@gmail.com>
2022-02-25 14:36:39 +1100

    implement product page

---
+++ src/components/productPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function productPage(props) {
+  return (
+    <div>productPage</div>
+  )
+}
+
+productPage.propTypes = {}
+
+export default productPage
